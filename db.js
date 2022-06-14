5500efdbc1f864ff15a3a220e44b0980d17cb45f
Grazielle M. Santiago <104645740+gramedsant@users.noreply.github.com>
2022-06-13 21:00:23 -0300

    initial commit

---
+++ db.js
@@ -0,0 +1,141 @@
+// db = DataBase
+
+async function conecta(){
+    const mysql = require("mysql2/promise")
+    const conn = await mysql.createConnection({
+        host: "localhost",
+        user: "seu_user",
+        password: "sua_senha",
+        database: "filmes"
+    })
+    console.log('mySQL conectado!')
+    global.connection = conn
+    return connection
+}
+
+//conecta()
+
+async function selectFilmes(){
+    const conectado = await conecta()
+    const [rows] = await conectado.query("SELECT f.titulo,f.genero,d.nome FROM videos as f INNER JOIN diretor as d ON f.diretor = d.diretor_id ORDER BY f.titulo ASC")
+    //console.log(rows)
+    return rows
+}
+
+async function selectLivros(){
+    const conectado = await conecta()
+    const [rows] = await conectado.query("SELECT * FROM livros ORDER BY livros_id DESC")
+    //console.log(rows)
+    return rows
+}
+
+async function selectSingle(id){
+    const conectado = await conecta()
+    const values = [id]
+    const [rows] = await conectado.query("SELECT * FROM livros WHERE livros_id=?",values)
+    //console.log(rows)
+    return rows
+}
+
+async function selectPromo(){
+    const conectado = await conecta()
+    const [rows] = await conectado.query("SELECT * FROM livros WHERE promo=1")
+    //console.log(rows)
+    return rows
+}
+
+async function selectUsers(email,senha){
+    const conectado = await conecta()
+    const values = [email,senha]
+    const [rows] = await conectado.query("SELECT * FROM usuarios WHERE email=? AND senha=?", values)
+    //console.log(rows)
+    return rows
+}
+
+async function updatePromo(id){
+    const conectado = await conecta()
+    const values = [promo,id]
+    return await conectado.query("UPDATE livros SET promo=? WHERE livros_id=?",values)
+}
+//updatePromo(1,3)
+
+async function deleteCarrinho(id){
+    const conectado = await conecta()
+    const values = [id]
+    return await conectado.query("DELETE FROM carrinho WHERE carrinho_id=?",values)
+}
+
+async function insertLivro(livro){
+    const conectado = await conecta()
+    const values = [livro.titulo,livro.resumo,livro.valor,livro.imagem]
+    const [rows] = 
+    await conectado.query("INSERT INTO livros(titulo,resumo,valor,imagem) VALUES (?,?,?,?)",values)
+    console.log("Insert OK")
+    return rows
+}
+
+async function insertContato(contato){
+    const conectado = await conecta()
+    const values = [contato.nome,contato.sobrenome,contato.email,contato.mensagem]
+    const [rows] = 
+    await conectado.query("INSERT INTO contato(nome,sobrenome,email,mensagem) VALUES (?,?,?,?)",values)
+    console.log("Insert OK")
+    return rows
+}
+
+async function insertCarrinho(carrinho){
+    const conectado = await conecta()
+    const values = [carrinho.produto,carrinho.qtd,carrinho.valor,carrinho.livros_id]
+    const [rows] = 
+    await conectado.query("INSERT INTO carrinho(produto,qtd,valor,livros_id) VALUES (?,?,?,?)",values)
+    console.log("Insert OK")
+    return rows
+}
+
+async function cadastroContato(usuarios){
+    const conectado = await conecta()
+    const values = [usuarios.nome,usuarios.email,usuarios.telefone,usuarios.senha,usuarios.conf_senha]
+    const [rows] = 
+    await conectado.query("INSERT INTO usuarios(nome,email,telefone,senha,conf_senha) VALUES (?,?,?,?,?)",values)
+    console.log("Insert OK")
+    return rows
+}
+// insertContato({
+//     nome:"Michael",
+//     sobrenome:"Jackson",
+//     email:"maiquin@gmail.com",
+//     mensagem:"hi hii"
+// })
+
+async function selectCarrinho(){
+    const conectado = await conecta()
+    const [rows] = await conectado.query("SELECT * FROM carrinho ORDER BY carrinho_id DESC")
+    //console.log(rows)
+    return rows
+}
+
+//selectFilmes()
+//selectLivros()
+//selectSingle(10)
+// insertLivro({titulo:"Wild Fury", resumo:"Lorem Resumo Wild Fire",valor:40.35,imagem:"wild-fury.jpg"})
+
+
+
+module.exports = {
+    selectFilmes,
+    selectLivros,
+    selectSingle,
+    selectCarrinho,
+    selectPromo,
+    selectUsers,
+    insertLivro,
+    updatePromo,
+    insertContato,
+    cadastroContato,
+    insertCarrinho,
+    deleteCarrinho
+    
+}
+
+
+
---
+++ index_ejs.js
@@ -0,0 +1,193 @@
+// const { render } = require('express/lib/response')
+
+(async () => {
+const express = require('express')
+const app = express()
+const db = require("./db.js")
+const url = require("url")
+const bodyParser = require("body-parser")
+const session = require("express-session")
+const port = 3000
+
+app.set('view engine','ejs')
+
+const dia = 1000 * 60 * 60 * 24;
+const min15 = 1000 * 60 * 60 / 4;
+
+
+app.use(session({
+    secret: "hrgfgrfrty84fwir767",
+    saveUninitialized:true,
+    cookie: { maxAge: dia},
+    resave: false 
+}))
+
+//Config para as variáveis post
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.json())
+
+app.use(express.static('livraria-2022'))
+app.use("/books",express.static('books'))
+app.use("/imgs",express.static('imgs'))
+app.use("/css",express.static('css'))
+app.use("/js",express.static('js'))
+
+const consulta = await db.selectFilmes()
+const consultaLivro = await db.selectLivros()
+//console.log(consulta[0])
+//console.log(consultaLivro[0])
+
+app.get("/login",async(req,res) => {
+    res.render(`login`, {
+        titulo:"Entrar - Livros Online"
+    })
+})
+
+app.post("/login",async(req,res)=>{
+    let info = req.body
+    let consultaUsers = await db.selectUsers(info.email,info.senha)
+    consultaUsers == '' ? res.send("Usuário Não Encontrado") : res.redirect("/")
+    const s = req.session
+    consultaUsers != '' ? s.nome = info.nome : null
+})
+
+app.get("/",(req,res) => {
+    res.render(`index`, {
+        titulo:"Conheça nossos livros", 
+        promo:"Todos os livros com 10%OFF !",
+        livro: consulta,
+        galeria: consultaLivro
+    })
+})
+
+app.get("/upd-promo",(req,res) => {
+    res.render(`admin/atualiza-promocoes`, {
+        titulo:"Conheça nossos livros", 
+        promo:"Todos os livros com 10%OFF !",
+        livro: consulta,
+        galeria: consultaLivro
+    })
+})
+
+app.get("/insere-livro",async(req,res) =>{
+   await db.insertLivro({titulo:"Guerra Dos Mundos", resumo:"Lorem loren",valor:20.45,imagem:"guerra-dos-mundos.jpg"})
+    res.send("<h2>livro Adicionado!</h2> <a href='./'>Voltar</a>")
+})
+
+app.get("/atualiza-promo",async(req,res) =>{
+    let qs = url.parse(req.url,true).query
+    await db.updatePromo(qs.promo,qs.id) //localhost:8080/atualiza-promo?promo=1&id=8
+     res.send("<h2>Lista de Promoções Atualizadas!</h2> <a href='./'>Voltar</a>")
+ })
+
+app.get("/promocoes",async (req,res) => {
+    const consultaPromo = await db.selectPromo()
+    res.render(`promocoes`, {
+        titulo:"Conheça nossos livros", 
+        promo:"Todos os livros com 10%OFF !",
+        livro: consulta,
+        galeria: consultaPromo
+    })
+})
+
+
+app.get("/single-produto",async(req,res) => {
+    let infoUrl = req.url
+    let urlProp = url.parse(infoUrl,true) // ?id=5
+    let q = urlProp.query
+    const consultaSingle = await db.selectSingle(q.id)
+    const consultaInit = await db.selectSingle(4)
+
+
+    res.render(`single-produto`, {
+        titulo:"Conheça nossos livros", 
+        promo:"Todos os livros com 10%OFF !",
+        livro: consulta,
+        galeria: consultaSingle,
+        inicio: consultaInit
+        })
+})
+
+app.get("/cadastro",async(req,res) => {
+    let infoUrl = req.url
+    let urlProp = url.parse(infoUrl,true) // ?id=5
+    let q = urlProp.query
+    const consultaSingle = await db.selectSingle(q.id)
+    const consultaInit = await db.selectSingle(4)
+
+    res.render(`cadastro`, {
+        titulo:"Conheça nossos livros", 
+        promo:"Todos os livros com 10%OFF !",
+        livro: consulta,
+        galeria: consultaInit
+        })
+})
+
+app.post("/cadastro",async(req,res)=> {
+    const info=req.body
+    await db.cadastroContato({
+    nome:info.nome,
+    email:info.email,
+    telefone:info.telefone,
+    senha:info.senha,
+    conf_senha:info.conf_senha
+})
+    res.redirect("/promocoes")
+})
+
+app.get("/contato",async(req,res) => {
+    let infoUrl = req.url
+    let urlProp = url.parse(infoUrl,true) // ?id=5
+    let q = urlProp.query
+    const consultaSingle = await db.selectSingle(q.id)
+    const consultaInit = await db.selectSingle(4)
+
+    res.render(`contato`, {
+        titulo:"Conheça nossos livros", 
+        promo:"Todos os livros com 10%OFF !",
+        livro: consulta,
+        galeria: consultaInit
+        })
+})
+
+app.post("/contato",async(req,res)=> {
+    const info=req.body
+    await db.insertContato({
+    nome:info.cad_nome,
+    sobrenome:info.cad_sobrenome,
+    email:info.cad_email,
+    mensagem:info.cad_mensagem
+})
+    res.redirect("/promocoes")
+})
+
+app.get("/carrinho", async(req,res) => {
+    const consultaCarrinho = await db.selectCarrinho()
+    res.render(`carrinho`, {
+        titulo:"Conheça nossos livros", 
+        promo:"Todos os livros com 10%OFF !",
+        livro: consulta,
+        carrinho: consultaCarrinho
+    })
+})
+
+app.post("/carrinho",async(req,res)=> {
+    const info =req.body
+    await db.insertCarrinho({
+        produto: info.produto,
+        qtd: info.qtd,
+        valor: info.valor,
+        livros_id: info.livros_id
+    })
+    res.send(req.body)
+})
+
+app.post("/delete-carrinho",async(req,res)=> {
+    const info =req.body
+    await db.deleteCarrinho(info.id)
+    res.send(info)
+})
+
+
+app.listen(port,() => console.log(`Servidor Rodando na porta ${port} com nodemon!`))
+})()
